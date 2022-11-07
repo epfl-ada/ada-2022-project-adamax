dcf02d21ed5e6ddb70233e2a5f03f1ad79e3f2e7
pirhona2 <anton.pirhonen@aalto.fi>
2022-11-07 18:08:55 +0100

    Move code to src folder

--- src/helpers.py
+++ src/helpers.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 
 def get_characters():
     columns = ["WikiID", "FreebaseID", "ReleaseDate", "CharacterName", "ActorDOB", "ActorGender", "ActorHeight", "ActorEthnicity", "ActorName", "ActorAge", "FreebaseCharacterActorMapId", "FreebaseCharacterID", "FreebaseActorID"]
-    return pd.read_csv('data/character.metadata.tsv', sep='\t', names=columns, index_col=False)
+    return pd.read_csv('../data/character.metadata.tsv', sep='\t', names=columns, index_col=False)
 
 def get_freebase_value(s):
     l = str.split(s, ':')
@@ -28,7 +28,7 @@ Genre = namedtuple("Genre", ["name", "id"])
 
 def get_movies():
     columns = ["WikiID", "FreebaseID", "Name", "ReleaseDate", "BoxOfficeRevenue", "Runtime", "Language", "Country", "Genres"]
-    movies = pd.read_csv('data/movie.metadata.tsv', sep='\t', names=columns, index_col=False)
+    movies = pd.read_csv('../data/movie.metadata.tsv', sep='\t', names=columns, index_col=False)
     languages = movies["Language"]
     movies["Language"] = languages.apply(get_freebase_value)
     movies["LanguageID"] = languages.apply(get_freebase_id)
@@ -44,4 +44,4 @@ def get_movies():
 
 def get_plot_summaries():
     columns = ["WikiID", "Plot"]
-    return pd.read_csv('data/plot_summaries.txt', sep='\t', names=columns, index_col=False)
+    return pd.read_csv('../data/plot_summaries.txt', sep='\t', names=columns, index_col=False)
