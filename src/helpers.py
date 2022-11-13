14d161604c5b214c22dc5eb2ab34af2b0d8a0d64
pirhona2 <anton.pirhonen@aalto.fi>
2022-11-13 22:11:56 +0100

    Analyse the corenlp results for father adjacent words

--- src/helpers.py
+++ src/helpers.py
@@ -1,8 +1,13 @@
 ### Helpers for the data processing
-import numpy as np
 import pandas as pd
-import cpi
+# import cpi # For inflation adjusted revenue
 from collections import namedtuple
+import json
+import xml.etree.ElementTree as ET
+import gzip
+import os
+import shutil
+
 
 movieSummariesPath = "../data/MovieSummaries"
 imdbPath = "../data/Imdb"
@@ -49,8 +54,8 @@ def get_movies():
 
     movies["ReleaseDate"] = pd.to_datetime(movies["ReleaseDate"])
     movies["Year"] = movies["ReleaseDate"].apply(lambda x: x.year)
-    movies["InflationAdjustedRevenue"] = movies.apply(
-        lambda movie: cpi.inflate(movie["BoxOfficeRevenue"], int(movie["Year"]), to=2021) if not np.isnan(movie["Year"]) and movie["Year"] >= 1913 else np.nan, axis=1)
+    # movies["InflationAdjustedRevenue"] = movies.apply(
+    #     lambda movie: cpi.inflate(movie["BoxOfficeRevenue"], int(movie["Year"]), to=2021) if not np.isnan(movie["Year"]) and movie["Year"] >= 1913 else np.nan, axis=1)
 
     return movies
 
@@ -80,3 +85,69 @@ def get_imdb_ratings_small():
 
 def get_imdb_ratings():
     return pd.read_csv(f'{imdbPath}/ratings.csv', index_col=False)
+
+# Unzip all the .gz compressed data in ../data/corenlp_plot_summaries folder
+# Assumes that the zipped files are present in the folder
+def unzip_corenlp_plot_summaries():
+    # Find all the .gz files in the data folder
+    rootdir = '../data/corenlp_plot_summaries'
+    gz_files = []
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            if file.endswith('.gz'):
+                gz_files.append(os.path.join(subdir, file))
+
+    # Unzip all the .gz files
+    for gz_file in gz_files:
+        with gzip.open(gz_file, 'rb') as f_in:
+            with open(gz_file[:-3], 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+    # Remove all the .gz files
+    for gz_file in gz_files:
+        os.remove(gz_file)
+
+
+def get_corenlp_father_words(count):
+    # Father reference words
+    father_reference_words = ["father", "dad", "papa"]
+
+    # Store the count of father related words
+    father_dependent_words = {}
+    father_governor_words = {}
+
+    # Parse and print out father information
+    def get_father_info(root):
+        for dep in root.iter('dep'):
+            if dep.find("governor").text in father_reference_words:
+                if dep.find("dependent").text in father_dependent_words:
+                    father_dependent_words[dep.find("dependent").text] += 1
+                else:
+                    father_dependent_words[dep.find("dependent").text] = 1
+
+            if dep.find("dependent").text in father_reference_words:
+                if dep.find("governor").text in father_governor_words:
+                    father_governor_words[dep.find("governor").text] += 1
+                else:
+                    father_governor_words[dep.find("governor").text] = 1
+                
+        
+    # Find all the xml file names in the xmls directory
+    rootdir = '../data/corenlp_plot_summaries'
+    xml_files = []
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            if file.endswith('.xml'):
+                xml_files.append(os.path.join(subdir, file))
+
+    x = 0
+    # Parse each xml file (count for short circuiting)
+    for xml_file in xml_files:
+        if x > count:
+            break
+        x += 1
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        get_father_info(root)
+        
+    return father_dependent_words, father_governor_words
